4a43fb160b73d1226d2a81f394c880ab478ec5b7
Emmanuel Ramirez <jsEmmanuelRamire@localhost.localdomain>
2018-05-25 16:02:07 -0500

    Modificaciones de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
+import { generate } from 'rxjs/observable/generate';
 import { NgTools_InternalApi_NG_2_LazyRouteMap } from '@angular/compiler-cli/src/ngtools_api';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modules
+import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
 import { APP_ROUTES } from './approutes';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './register/register.component';
 
 
@@ -22,19 +17,12 @@ import { RegisterComponent } from './register/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
